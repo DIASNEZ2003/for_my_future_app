b823d9e2c23cd58f23984cf3b12546eac6bb57eb
DIASNEZ2003 <kramoninaor@gmail.com>
2026-01-28 19:16:04 +0800

    App

---
+++ frontend/app/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const home = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-xl font-semibold'>Mao ni Home baby with Tabs baby</Text>
+    </View>
+  )
+}
+
+export default home
